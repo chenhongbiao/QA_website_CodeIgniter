1ffcab11b61c9b169caa5fdeb4f6d957b425f873
faustmeow <faustmeow@gmail.com>
2016-06-09 23:42:13 +0800

    add header and footer

--- CodeIgniter-3.0.6/application/views/templates/footer.php
+++ CodeIgniter-3.0.6/application/views/templates/footer.php
@@ -1,8 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-	<p class="footer">This Page <?php echo "@Meow" ?></p>
+
 </div> <!--the end of <div id="container"> which open in the header-->
+
+<div class="footer">
+    <p class="footer">This Page <?php echo "@Meow" ?></p>
+</div>
 
 </body>
 </html>
--- CodeIgniter-3.0.6/application/views/templates/header.php
+++ CodeIgniter-3.0.6/application/views/templates/header.php
@@ -13,4 +13,21 @@ $this->load->helper('url');
 </head>
 <body>
 
+<div id="header-container">
+
+<div class="header">
+        <div class="pure-menu pure-menu-horizontal">
+            <a class="pure-menu-heading" href="">QAs</a>
+
+            <ul class="pure-menu-list">
+                <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
+                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Blog</a></li>
+                <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
+            </ul>
+
+        </div>
+    </div>
+
+</div>
+
 <div id="container">
