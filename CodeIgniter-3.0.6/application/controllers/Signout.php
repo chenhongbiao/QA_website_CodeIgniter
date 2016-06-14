735a8b472282fe1f70431ecb7475bd5da39fc8d2
faustmeow <faustmeow@gmail.com>
2016-06-14 21:57:44 +0800

    add sign out

---
+++ CodeIgniter-3.0.6/application/controllers/Signout.php
@@ -0,0 +1,17 @@
+<?php
+class Signout extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url_helper');
+        $this->load->library('session');
+    }
+    
+    public function index()
+{
+    session_destroy();
+    header ("Location: questions");
+}
+
+}
--- CodeIgniter-3.0.6/application/views/templates/includes/session.php
+++ CodeIgniter-3.0.6/application/views/templates/includes/session.php
@@ -1,5 +1,6 @@
 <?php
    $this->load->library('session');
+   //one in the view layer - and one in the controller layer
 
     function logged_on()
     {
